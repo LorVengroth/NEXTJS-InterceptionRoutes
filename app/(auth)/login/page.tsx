29e9513850e4fba7bb0ed71d601d7f8105ebdd62
luoyonglun1 <vengroth3322@gmail.com>
2026-01-08 22:30:05 +0700

    move

--- app/(auth)/login/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function LoginPage(){
-    return(
-        <div>
-            <h1>This is how we use routes group the URL didnt show the (auth) folder </h1>
-            <h2>This is Login Page </h2>
-        </div>
-    )
-}
--- app/_lib/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Private(){
-    return (
-        <h1> You cant view this page in brower cuz it the private folder</h1>
-    )
-}
